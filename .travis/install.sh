c571812c1ce30b115966a9bb102f0ddbb784086b
KOLANICH <kolan_n@mail.ru>
2022-06-09 14:38:45 +0300

    Moved the metadata from `setup.py` to `setup.cfg`.

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,8 @@ in
     run_tests|coverage)
         if [ "2.6" = "$TRAVIS_PYTHON_VERSION" ]; then pip install unittest2; fi
         if [ "coverage" = $TEST_MODE ]; then pip install coverage codecov; fi
-        python setup.py install
+        pip install --upgrade setuptools wheel pip setuptools_scm build
+        python -m build -nwx
         ;;
     check_style)
         pip install flake8
